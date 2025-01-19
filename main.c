93e8b437bcdafb02d4a563fda740f29d9aac39f9
Rick0202 <richard.harootunian@gmail.com>
2025-01-19 17:15:47 +0000

    Saving Program

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  double windSpeed = 0;
+
+  printf("The Saffir-Simpson Hurricane Scale classifies hurricanes into five categories numbered 1 through 5.\nThis program will classify the type of storm based upon the users input of the wind speed. \n\nEnter the wind speed: ");
+  
+  scanf("%lf", &windSpeed);
+
+  if (windSpeed < 74) {
+    printf("\nThis is not a hurricane.\nThe wind speed is: %.3lf: ", windSpeed);
+  }
+
+  if (windSpeed >= 74 && windSpeed < 96) {
+    printf("\nThis is a category 1 hurricane.\nThe wind speed is: %.3lf: ", windSpeed);
+  }
+
+  if (windSpeed >= 96 && windSpeed < 111) {
+    printf("\nThis is a category 2 hurricane.\nThe wind speed is: %.3lf: ", windSpeed);
+  }
+  if (windSpeed >= 111 && windSpeed < 130) {
+    printf("\nThis is a category 3 hurricane.\nThe wind speed is: %.3lf: ", windSpeed);
+  }
+  if (windSpeed >= 130 && windSpeed < 157) {
+    printf("\nThis is a category 4 hurricane.\nThe wind speed is: %.3lf: ", windSpeed);
+  }
+  if (windSpeed >=157) {
+    printf("\nThis is a category 5 hurricane.\nThe wind speed is: %.3lf: ", windSpeed);
+  }
   return 0;
 }
